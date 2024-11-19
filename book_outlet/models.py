ce3dea3ade7775b6d340b84a6bd26b1400b8e5b6
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2024-11-19 10:23:13 +0530

    preparing templates

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator , MaxValueValidator
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxValueValidator(5)])
+    author = models.CharField(max_length=50)
+    is_bestselling =  models.BooleanField()
+    
+    
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
